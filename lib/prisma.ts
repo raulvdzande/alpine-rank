3ea7a612c41344c426abca809d3084762b2f5bf5
raulvdzande <raulvdzande740@gmail.com>
2026-02-25 19:25:45 +0100

    added pool

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
-const adapter = new PrismaPg(process.env.DATABASE_URL!);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
 
 const globalForPrisma = globalThis as typeof globalThis & {
   prisma?: PrismaClient;
